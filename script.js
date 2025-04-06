0650d84e7dfb18ced11e149d5cebe003059079b7
SynthEDU <65percenthax@gmail.com>
2025-04-06 18:03:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const sideMenu = document.getElementById('sideMenu');
+    if (sideMenu.style.left === '0px') {
+        sideMenu.style.left = '-250px'; 
+    } else {
+        sideMenu.style.left = '0'; 
+    }
+}
